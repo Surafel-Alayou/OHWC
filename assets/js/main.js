13a36b432cfc2d7f67100210ab87a9b7924198de
Surafel_Alayou <surafel2alayou@gmail.com>
2023-12-14 08:43:52 +0300

    Major changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,28 +146,28 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Porfolio isotope and filter
    */
-  let portfolionIsotope = document.querySelector('.portfolio-isotope');
+  let gallerynIsotope = document.querySelector('.gallery-isotope');
 
-  if (portfolionIsotope) {
+  if (gallerynIsotope) {
 
-    let portfolioFilter = portfolionIsotope.getAttribute('data-portfolio-filter') ? portfolionIsotope.getAttribute('data-portfolio-filter') : '*';
-    let portfolioLayout = portfolionIsotope.getAttribute('data-portfolio-layout') ? portfolionIsotope.getAttribute('data-portfolio-layout') : 'masonry';
-    let portfolioSort = portfolionIsotope.getAttribute('data-portfolio-sort') ? portfolionIsotope.getAttribute('data-portfolio-sort') : 'original-order';
+    let galleryFilter = gallerynIsotope.getAttribute('data-gallery-filter') ? gallerynIsotope.getAttribute('data-gallery-filter') : '*';
+    let galleryLayout = gallerynIsotope.getAttribute('data-gallery-layout') ? gallerynIsotope.getAttribute('data-gallery-layout') : 'masonry';
+    let gallerySort = gallerynIsotope.getAttribute('data-gallery-sort') ? gallerynIsotope.getAttribute('data-gallery-sort') : 'original-order';
 
     window.addEventListener('load', () => {
-      let portfolioIsotope = new Isotope(document.querySelector('.portfolio-container'), {
-        itemSelector: '.portfolio-item',
-        layoutMode: portfolioLayout,
-        filter: portfolioFilter,
-        sortBy: portfolioSort
+      let galleryIsotope = new Isotope(document.querySelector('.gallery-container'), {
+        itemSelector: '.gallery-item',
+        layoutMode: galleryLayout,
+        filter: galleryFilter,
+        sortBy: gallerySort
       });
 
-      let menuFilters = document.querySelectorAll('.portfolio-isotope .portfolio-flters li');
+      let menuFilters = document.querySelectorAll('.gallery-isotope .gallery-flters li');
       menuFilters.forEach(function(el) {
         el.addEventListener('click', function() {
-          document.querySelector('.portfolio-isotope .portfolio-flters .filter-active').classList.remove('filter-active');
+          document.querySelector('.gallery-isotope .gallery-flters .filter-active').classList.remove('filter-active');
           this.classList.add('filter-active');
-          portfolioIsotope.arrange({
+          galleryIsotope.arrange({
             filter: this.getAttribute('data-filter')
           });
           if (typeof aos_init === 'function') {
