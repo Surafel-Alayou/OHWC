60516849c1ef3a896fa7d64311262b8421c2e4fd
Surafel-Alayou <surafel2alayou@gmail.com>
2024-01-08 16:08:22 +0300

    Message

---
+++ assets/js/message.js
@@ -0,0 +1,25 @@
+    function sendMessage() {
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var subject = document.getElementById('subject').value;
+        var message = document.getElementById('message').value;
+
+        var botToken = '5395660536:AAERYCKuPCvSqXjb_f9ccKwvnsw0bJdCUA4';
+        var chatId = '@AMSE_lab';
+
+        var text = "New message from:\nName: " + name + "\nEmail: " + email + "\nSubject: " + subject + "\nMessage: " + message;
+
+        var url = "https://api.telegram.org/bot" + botToken + "/sendMessage?text=" + encodeURIComponent(text) + "&chat_id=" + chatId;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.send();
+
+        xhr.onload = function() {
+            if (xhr.status == 200) {
+                alert("Message sent successfully!");
+            } else {
+                alert("Error sending message.");
+            }
+        };
+    }
